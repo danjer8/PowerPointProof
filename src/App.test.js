d1fe15099c0764911cbb82a291f4445d48234107
daniel <Daniel.Jerabek@tum.de>
2023-11-09 11:56:04 +0100

    changed tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/PowerPoint Analyse Tool/i);
   expect(linkElement).toBeInTheDocument();
 });
